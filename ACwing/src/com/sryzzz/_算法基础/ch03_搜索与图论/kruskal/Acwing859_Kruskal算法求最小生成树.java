68a4d8db0bf1672cc5662ca839a84cc65d2d8cde
sryzzz <1299975117@qq.com>
2022-09-05 22:43:46 +0800

    feat: 完成 Acwing859_Kruskal算法求最小生成树

---
+++ ACwing/src/com/sryzzz/_算法基础/ch03_搜索与图论/kruskal/Acwing859_Kruskal算法求最小生成树.java
@@ -0,0 +1,88 @@
+package com.sryzzz._算法基础.ch03_搜索与图论.kruskal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+import java.util.Arrays;
+
+/**
+ * Kruskal算法求最小生成树
+ *
+ * @author sryzzz
+ * @create 2022/9/5 22:43
+ * @description https://www.acwing.com/problem/content/description/861/
+ */
+public class Acwing859_Kruskal算法求最小生成树 {
+    static int n, m;
+    static Edge[] e;
+    static int[] fa;
+
+    public static void main(String[] args) throws IOException {
+        StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        in.nextToken();
+        n = (int) in.nval;
+        in.nextToken();
+        m = (int) in.nval;
+        e = new Edge[m + 1];
+        fa = new int[n + 1];
+        for (int i = 1; i <= m; i++) {
+            e[i] = new Edge();
+        }
+        for (int i = 1; i <= m; i++) {
+            in.nextToken();
+            e[i].u = (int) in.nval;
+            in.nextToken();
+            e[i].v = (int) in.nval;
+            in.nextToken();
+            e[i].w = (int) in.nval;
+        }
+
+        int ans = kruskal();
+        if (ans == -1) {
+            System.out.println("impossible");
+        } else {
+            System.out.println(ans);
+        }
+
+    }
+
+    public static int find(int x) {
+        if (fa[x] != x) {
+            fa[x] = find(fa[x]);
+        }
+        return fa[x];
+    }
+
+    public static int kruskal() {
+        int ans = 0, numE = 0;
+        for (int i = 1; i <= n; i++) fa[i] = i;
+        Arrays.sort(e, 1, e.length);
+        for (int i = 1; i <= m; i++) {
+            int faU = find(e[i].u);
+            int faV = find(e[i].v);
+            if (faU != faV) {
+                fa[faU] = faV;
+                ans += e[i].w;
+                numE++;
+                if (numE == n - 1) {
+                    break;
+                }
+            }
+        }
+        if (numE != n - 1) {
+            return -1;
+        } else {
+            return ans;
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int u, v, w;
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.w - o.w;
+        }
+    }
+}
